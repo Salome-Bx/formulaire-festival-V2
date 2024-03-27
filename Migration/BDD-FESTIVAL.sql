abbefe1141a8d0bc67fecf1cc77083a74c35d498
Clément Saulle <saulle45@gmail.com>
2024-03-27 09:33:23 +0100

    BDD focntionnel

--- Migration/BDD-FESTIVAL.sql
+++ Migration/BDD-FESTIVAL.sql
@@ -14,7 +14,7 @@ CREATE TABLE festival_USER(
         password  Varchar (250) NOT NULL ,
         address   Varchar (250) NOT NULL ,
         telephone Int NOT NULL ,
-        User_Role Bool NOT NULL ,
+        User_Role TINYINT(1) NOT NULL ,
         mail      Varchar (50) NOT NULL
 	,CONSTRAINT USER_AK UNIQUE (mail)
 	,CONSTRAINT USER_PK PRIMARY KEY (Id_User)
@@ -30,11 +30,11 @@ CREATE TABLE festival_Reservation(
         Number_Reservation Int NOT NULL ,
         Quantity_Sledge    Int NOT NULL ,
         Quantity_Headphone Int NOT NULL ,
-        Children           Bool NOT NULL ,
+        Children           TINYINT(1) NOT NULL ,
         Id_User            Int NOT NULL
 	,CONSTRAINT Reservation_PK PRIMARY KEY (ID_RESERVATION)
 
-	,CONSTRAINT Reservation_USER_FK FOREIGN KEY (Id_User) REFERENCES USER(Id_User)
+	,CONSTRAINT Reservation_USER_FK FOREIGN KEY (Id_User) REFERENCES festival_USER(Id_User)
 )ENGINE=InnoDB;
 
 
@@ -73,8 +73,8 @@ CREATE TABLE festival_ReservationHasEvent(
         ID_RESERVATION Int NOT NULL
 	,CONSTRAINT ReservationHasEvent_PK PRIMARY KEY (Id_Date,ID_RESERVATION)
 
-	,CONSTRAINT ReservationHasEvent_Event_FK FOREIGN KEY (Id_Date) REFERENCES Event(Id_Date)
-	,CONSTRAINT ReservationHasEvent_Reservation0_FK FOREIGN KEY (ID_RESERVATION) REFERENCES Reservation(ID_RESERVATION)
+	,CONSTRAINT ReservationHasEvent_Event_FK FOREIGN KEY (Id_Date) REFERENCES festival_Event(Id_Date)
+	,CONSTRAINT ReservationHasEvent_Reservation0_FK FOREIGN KEY (ID_RESERVATION) REFERENCES festival_Reservation(ID_RESERVATION)
 )ENGINE=InnoDB;
 
 
@@ -88,7 +88,7 @@ CREATE TABLE festival_ReservationHasNight(
         Date           Date NOT NULL
 	,CONSTRAINT ReservationHasNight_PK PRIMARY KEY (Id_Date,ID_RESERVATION)
 
-	,CONSTRAINT ReservationHasNight_Night_FK FOREIGN KEY (Id_Date) REFERENCES Night(Id_Date)
-	,CONSTRAINT ReservationHasNight_Reservation0_FK FOREIGN KEY (ID_RESERVATION) REFERENCES Reservation(ID_RESERVATION)
+	,CONSTRAINT ReservationHasNight_Night_FK FOREIGN KEY (Id_Date) REFERENCES festival_Night(Id_Date)
+	,CONSTRAINT ReservationHasNight_Reservation0_FK FOREIGN KEY (ID_RESERVATION) REFERENCES festival_Reservation(ID_RESERVATION)
 )ENGINE=InnoDB;
 
