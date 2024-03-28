b1c90626a25ca59ade3c96b829bdc367c2f7b789
Killian <Killian2908@gmail.com>
2024-03-28 10:02:54 +0100

    CRUD Reservation

--- src/Repository/ReservationRepository.php
+++ src/Repository/ReservationRepository.php
@@ -5,8 +5,8 @@ use src\Models\Database;
 
 class ReservationRepository
 {
-    private $DB;
-    private $resa;
+    private Database $DB;
+    private Reservation $resa;
     public function __construct()
     {
 
@@ -16,16 +16,68 @@ class ReservationRepository
 
         require_once __DIR__ . '/../../config.php';
     }
+    //* Create Reservation in Database
     function putReservationInDB(): bool
     {
         $sql = "INSERT INTO festival_reservation (ID_RESERVATION, Number_Reservation, Quantity_Sledge, Quantity_Headphone, Children, Id_User) VALUES (:ID_RESERVATION, :Number_Reservation, :Quantity_Sledge, :Quantity_Headphone, :Children, :Id_User)";
 
+
         $statement = $this->DB->getDB()->prepare($sql);
-        $statement->execute([":ID_RESERVATION" => $this->resa->getIdReservation()]);
+        $reservation = $this->resa;
+        $statement->execute([
+            ":ID_RESERVATION" => $reservation->getIdReservation(),
+            ":Number_Reservation" => $reservation->getQuantitySledge(),
+            ":Quantity_Sledge" => $reservation->getQuantitySledge(),
+            "Quantity_Headphone" => $reservation->getQuantityHeadphone(),
+            ":Children" => $reservation->getChildren(),
+            "iD_User" => $reservation->getIdUser()
+        ]);
         return $statement->fetch(PDO::FETCH_ASSOC);
     }
 
-    function getReservationFromDB(): Returntype
+
+    //* Read Reservation from Database
+    function getReservationFromDB($Id_User): array
     {
+        $sql = "SELECT * FROM festival_reservation WHERE :Id_User";
+
+
+        $statement = $this->DB->getDB()->prepare($sql);
+        $statement->execute([
+            ":Id_User" => $Id_User
+        ]);
+        return $statement->fetch(PDO::FETCH_ASSOC);
     }
+
+    //* Edit Reservation in Database
+    function edtiReservationInDB($Id_User): bool
+    {
+        $sql = "UPDATE festival_reservation SET Number_Reservation = :Number_Reservation, Quantity_Sledge = :Quantity_Sledge, Quantity_Headphone = :Quantity_Headphone, Children = :Children WHERE Id_User = :Id_User";
+
+        $statement = $this->DB->getDB()->prepare($sql);
+        $reservation = $this->resa;
+        $statement->execute([
+            ":Number_Reservation" => $reservation->getQuantitySledge(),
+            ":Quantity_Sledge" => $reservation->getQuantitySledge(),
+            ":Quantity_Headphone" => $reservation->getQuantityHeadphone(),
+            ":Children" => $reservation->getChildren(),
+            ":Id_User" => $Id_User
+        ]);
+        return $statement->rowCount() > 0;
+    }
+
+
+    //* Delete Reservation in Database
+    function deleteReservationInDB($Id_User): bool
+    {
+        $sql = "DELETE FROM festival_reservation WHERE Id_User = :Id_User";
+
+        $statement = $this->DB->getDB()->prepare($sql);
+        $reservation = $this->resa;
+        $statement->execute([
+            ":Id_User" => $Id_User
+        ]);
+        return $statement->rowCount() > 0;
+    }
+
 }
