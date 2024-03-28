b8a39a994625b65c2ce66f12f0890fe5e413bcaa
Salome-Bx <salome.burteaux.simplon@gmail.com>
2024-03-28 16:05:15 +0100

    resarepo, usercontro

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -37,42 +37,9 @@ class UserController
             }
         }
         $user = new User($data);
-        if (isset($data['id_categories']) && !empty($data['id_categories'])) {
-            $user > setIdCategories($data['id_categories']);
-        }
 
-        if (
-            !empty($user->getNom()) &&
-            !empty($user->getUrlAffiche()) &&
-            !empty($user->getLienTrailer()) &&
-            !empty($user->getResume()) &&
-            !empty($user->getDuree()) &&
-            !empty($user->getDateSortie()) &&
-            !empty($user->getIdClassification())
-        ) {
-
-            if ($id !== null) {
-                $film->setId($id);
-                $this->FilmRepo->updateThisFilm($film);
-
-                $this->FilmRepo->removeFilmToCategories($film);
-                $this->FilmRepo->addFilmToCategories($film);
-            } else {
-                $film = $this->FilmRepo->CreateThisFilm($film);
-                $this->FilmRepo->addFilmToCategories($film);
-            }
-            header('location: /dashboard/films/details/' . $film->getId());
-            die;
-        } else {
-            $categories = $this->CategoryRepo->getAllCategories();
-            $classifications = $this->ClassificationRepo->getAllClassifications();
-            if ($id !== null) {
-                $this->render('Dashboard', ['section' => 'films', 'action' => 'edit', 'film' => $film, 'categories' => $categories, 'classifications' => $classifications, 'error' => 'Tous les champs sont requis.']);
-                die;
-            } else {
-                $this->render('Dashboard', ['section' => 'films', 'action' => 'new', 'film' => $film, 'categories' => $categories, 'classifications' => $classifications, 'error' => 'Tous les champs sont requis.']);
-                die;
-            }
+        if (isset($data['Id_User']) && !empty($data['Id_User'])) {
+            $this->UserRepo->saveUser($user['Id_User']);
         }
     }
 }
