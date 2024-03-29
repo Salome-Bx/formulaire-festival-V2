2275ffd835a4f7ce7ce4ad6c61f31e222add7e1f
Salome-Bx <salome.burteaux.simplon@gmail.com>
2024-03-29 09:26:44 +0100

    includes headers et footers dans views

--- config.php
+++ config.php
@@ -2,16 +2,16 @@
 // lors de la mise en open source, remplacer les infos concernant la base de données.
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'festival');
-define('DB_USER', 'festival');
-define('DB_PWD', 'festival');
-define('PREFIXE', 'festival_');
+// define('DB_NAME', 'festival');
+// define('DB_USER', 'festival');
+// define('DB_PWD', 'festival');
+// define('PREFIXE', 'festival_');
 
 define('HOME_URL', '/');
-// define('DB_NAME', 'festival');
-// define('DB_USER', 'root');
-// define('DB_PWD', '');
-// define('PREFIXE', 'festival_');
+define('DB_NAME', 'festival');
+define('DB_USER', 'root');
+define('DB_PWD', '');
+define('PREFIXE', 'festival_');
 // Ne pas toucher :
 
 define('DB_INITIALIZED', TRUE);
--- src/Views/inscriptionUser.php
+++ src/Views/inscriptionUser.php
@@ -1,54 +1,37 @@
-<?php
-?>
-<!DOCTYPE html>
-<html lang="en">
+<?php include __DIR__ . '/includes/header.php'; ?>
+<form action="inscription" id="inscriptionUser" method="POST">
+    <div id="coordonnees" class="blocFormulaire">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire d'inscription</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../../Public/assets/script.js">
-    <link rel="stylesheet" href="../../Public/assets/responsive.css">
-</head>
-
-<body>
-    <?php include __DIR__ . '/includes/header.php'; ?>
-    <form action="inscription" id="inscriptionUser" method="POST">
-        <div id="coordonnees" class="blocFormulaire">
-
-            <h2>Coordonnées</h2>
-            <div class="messageErreurChampsVides">
-                <?php if (!empty($_GET['erreur'])) {
-                    if ($_GET['erreur']) {
-                        echo ("Formulaire incorrect");
-                    }
-                };  ?>
-            </div>
-
-            <label for="nom">Nom :</label>
-            <input type="text" name="nom" id="nom" value="Dupont">
-            <label for=" prenom">Prénom :</label>
-            <input type="text" name="prenom" id="prenom" value="Pierre">
-            <label for="email">Email :</label>
-            <input type="email" name="email" id="email" value="email@gmail.com">
-            <label for="telephone">Téléphone :</label>
-            <input type=" text" name="telephone" id="telephone" value="0612345678">
-            <label for="adressePostale">Adresse Postale :</label>
-            <input type="text" name="adressePostale" id="adressePostale" value="4 rue Victor Hugo 38000 Grenoble">
-            <label for="password">Mot de passe :</label>
-            <input type="password" name="password" id="password" value="azdazdazd">
-            <label for="passwordBis">Confirmer votre mot de passe :</label>
-            <input type="password" name="passwordBis" id="passwordBis" value="azdazdazd">
-
-            <input type="submit" class="bouton" value="s'inscrire">
+        <h2>Coordonnées</h2>
+        <div class="messageErreurChampsVides">
+            <?php if (!empty($_GET['erreur'])) {
+                if ($_GET['erreur']) {
+                    echo ("Formulaire incorrect");
+                }
+            };  ?>
         </div>
-    </form>
-</body>
 
-<!-- <script src="./script.js"></script>
-<script src="./traitement.js"></script> -->
+        <label for="nom">Nom :</label>
+        <input type="text" name="nom" id="nom" value="Dupont">
+        <label for=" prenom">Prénom :</label>
+        <input type="text" name="prenom" id="prenom" value="Pierre">
+        <label for="email">Email :</label>
+        <input type="email" name="email" id="email" value="email@gmail.com">
+        <label for="telephone">Téléphone :</label>
+        <input type=" text" name="telephone" id="telephone" value="0612345678">
+        <label for="adressePostale">Adresse Postale :</label>
+        <input type="text" name="adressePostale" id="adressePostale" value="4 rue Victor Hugo 38000 Grenoble">
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" id="password" value="azdazdazd">
+        <label for="passwordBis">Confirmer votre mot de passe :</label>
+        <input type="password" name="passwordBis" id="passwordBis" value="azdazdazd">
+
+        <input type="submit" class="bouton" value="s'inscrire">
+    </div>
+</form>
+</body>
+<?php include __DIR__ . '/includes/footer.php'; ?>
+<script src="./script.js"></script>
+<script src="./traitement.js"></script>
 
 </html>
