cb36da07d4dbafba8e318abdaa4cdfe5de88660e
Salome-Bx <salome.burteaux.simplon@gmail.com>
2024-03-29 09:02:02 +0100

    header et footer

--- src/Views/includes/header.php
+++ src/Views/includes/header.php
@@ -1,5 +1,22 @@
-<?php ?>
-<header class="header">
-    <a href="./index.php" class="boutonConnexion">Formulaire</a>
-    <h1>Vercors Musique Festival</h1>
-</header>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../Public/assets/script.js">
+    <link rel="stylesheet" href="../../Public/assets/responsive.css">
+</head>
+
+<body>
+
+
+    <?php ?>
+    <header class="header">
+        <a href="./index.php" class="boutonConnexion">Formulaire</a>
+        <h1>Vercors Musique Festival</h1>
+    </header>
